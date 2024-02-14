de8c2e5a7b07896cd353e9bdccfaca511f573983
Nematov Begzod <nematovbegzodb@gmail.com>
2024-02-14 15:15:09 +0500

    Javascript-intro

---
+++ Javascript intro/script.js
@@ -0,0 +1,97 @@
+//! Javascript nima ? u qanday til va u bilan qanday dasturlar yoza olamiz
+
+//! Javascript
+
+// ? JavaScript - Bu veb sahifalarda murakkab, interaktiv funksiyalarni amalga oshirish imkonini beruvchi dasturlash tilidir
+// * JavaScript 1995-yilda amerikalik dasturchi Brendan Eich tomonidan yaratilgan. JavaScript tili barcha brauzerlar tomonidan ishlatiladi.
+
+// Javascript Bu - Web site Frontend Qismini Dinamik hodisalarini taminlaydi
+
+// Javascript 1995-yilda Netscape Kompanyasi Dasturchisi Brendan Eich tomonidan yaratilgan
+
+/* 
+    JavaScript avval Mocha, undan so'ng LiveScript tili sifatida ko'rinish bergan, lekin u muvaffaqiyatsiz yakunlangandan so'ng, marketing sabab, nomi Java script
+    o'zgartirilgan. Chunki o'sha payt Java dasturlash tili juda ommalashgan edi.
+
+*/
+
+// * O'shandan beri JavaScript Mozilla Firefox va Google Chrome kabi yangi brauzerlar qatorida o'sishda davom etdi. Hatto V8 deb nomlangan birinchi zamonaviy JavaScript dvigatelini ishlab chiqishni boshladi, so'ng spider monkey.
+
+// * Avval faqat brawserlarda ishlashga mo'ljallangan Javascript tili noje.js dvigateli ishlab chiqilgandan so'ng to'g'ridan to'g'ri Javascript kodini  mashina kodiga kompilyatsiya qiladi.
+
+/* 
+
+* Biz Javascript bilan nima qila olamiz?
+1) Web Apps (Web ilovalar): // ReactJs Angular js, Vue js,...
+2) Mobile Apps (Mobil ilovalar): //React native, Ionic
+3) Desktop Apps (Shaxsiy kompyuter uchun ilovalar): Electron js.
+4) Machine Learning: //TensorFlowJs, Brain js.
+5) Real-time Networking Apps: //online chats,
+meetings
+6) Games (o'yinlar):// GDevelop., melonJS., ImpactJS
+o https://bruno-simon.com/
+o http://hexgl.bkcore.com/play/
+*/
+
+/* 
+
+JavaScript kodi brauzerda yoki brauzerdan
+tashqarida ham ishga tushishi mumkin!
+
+JavaScript Engine (brauzerda)
+     🚀 FireFox -> "SpiderMonkey"
+     🚀 Google -> "V8"
+Brauzerdan tashqarida
+     Node js. 2009-yilda chiqarildi.
+     Deno js.
+
+*/
+// * Bugungi kunda JavaScriptda AngularJS , Vue va ReactJS kabi murakkab loyihalarni soddalashtirish uchun juda ko'p fremwork va kutubxonalar mavjud
+
+/* 
+
+Setup Development Environment (Dasturlash muhitini sozlash)
+* Instaling Visual Studio Code (VSCode):
+     https://code.visualstudio.com/download
+* Installing Node js :
+     https://nodejs.org/en/download/
+* Google Chrome:
+     https://google.com/chrome
+*  Git bash:
+     https://git-scm.com/downloads
+*/
+
+
+/* 
+? Setup VScode extensions (VScode extension larine o'rnatish)
+
+* Auto rename tag – tag nomini avtomatik o'zgartirish
+* Auto complete tag – avtomatik to'ldirib beradi tagni
+* Code runner – kodni terminalda ishlashida yordam beradi.
+* CSS peek – class ga olib borsak, qanday stillar berilganligi ko'rinadi.
+* IntelliSense for CSS class names in HTML – css yo'l ko'rsatkich
+* JavaScript(ES6) code snippets – ES6 extension
+* Live Server – local serverda jonli tarzda ochish
+* Path AutoComplete – fayllarimizga olib boradigan yo'lni ko'rsatadi.
+* Prettier code formatter – kodimizni chiroyli tartiblab beradi.
+* Color Highlight – CSS ranglarni o'z rangida yoritib beradi.
+* Material Icon Theme – papkaga ikonka qo'shadi.
+* Quokka js. – live js server
+* Community Material Theme – temani o'zgartirib beradi.
+
+*/
+
+// Javascript eng katta library(Kutubxonalari) va Frameworkli bu:
+// librarys(Kutubxonalari) --- VueJs, ReactJs
+// Frameworks --- Angular
+
+// library(Kutubxonalari) va Framework nega kerak -
+// Javascript qilinadigan ishni yengillashtirish uchun kerak
+
+// library(Kutubxona) va Framework Farqi - Fremwork Kutubxonadan katta
+// bo'ladi va bir nechta kutubxonalari bo'ladi
+
+// JavaScript veb - saytlarga interaktivlikni qo'shish, mobil ilovalar,
+// ish stoli ilovalari,
+// o'yinlar ishlab chiqish uchun
+// ishlatiladi va hozirda JavaScript mashinani organish va AI uchun ishlatilishi mumkin

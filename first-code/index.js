6a778ffa0d02e71ace9b7c0ccd185f41ff41a138
Nematov Begzod <nematovbegzodb@gmail.com>
2024-02-20 23:45:54 +0500

    Add first code file

---
+++ first-code/index.js
@@ -0,0 +1,20 @@
+console.log("Ansor"); // Browser > console > Ansor
+console.log(8 + 5); // Browser > console > 13
+console.log(1 > -1); // true
+console.log("nothing", "Ansor"); // => console.log() - Bu javascriptdagi funksiya bo'lib Browser'ning console qismiga biz istagan narsani (kod) chiqaradi
+// console - Browserning javascript kodlari yoziladigan va chiqarilagan terminali
+alert("Bu ogohlantirish"); // Browser oynasida chiqadigan ogohlantirish
+
+// JAVASCRIPT - BU DASTURLASH TILI (PROGRAMMING LANGUAGE)
+// ECMAScript - BU JAVASCRIPT VERSIYASI (SPECIFICATION)
+
+// qanday kod yozish kerak ?
+// Dasturlash nima - Dasturlash bu qurulmaga bizga kerakli amallarni bajarishi uchun so'z va sonlar bilan shu qurilmaga tushintirish
+// Bu so'zlar kodlar deb ataladi. kod - bu dasturlarni tashkil etuvchi matn
+// har qaysi dasturlash tili o'zi tushinadigan kodlari mavjuddir
+
+alert("Hello"); alert("world"); // Har bir operatsiyadan so'ng nuqta vergul qo'yish kerak ";"
+
+// kodni yanada o'qilishi uchun bayonotlar alohida satrlarda yoziladi:
+alert("Hello");
+alert("world");

553f0496c1e8f829db2d1a88522805b11b2503d7
Nematov Begzod <nematovbegzodb@gmail.com>
2024-02-23 22:23:53 +0500

    day 2

---
+++ day --- 2/script.js
@@ -0,0 +1,20 @@
+//! Javascript file yaratish:
+// Javascript file yaratish uchun .js kengaytmasidan foydalanish kerak
+
+// Qoidaga ko'ra, HTMLga faqat eng oddiy scriptlar qo'yiladi. Keyinchalik murakkablari alohida fayllarda joylashgan.
+
+// Alohida faylning afzalligi shundaki, brauzer uni yuklab oladi va keshda saqlaydi .
+// Xuddi shu skriptga havola qilingan boshqa sahifalar uni yuklab olish o'rniga keshdan oladi, shuning uchun fayl aslida bir marta yuklab olinadi. Bu trafikni kamaytiradi va sahifalarni tezroq qiladi.
+
+//* Javascriptni Ulash
+//! 3 - xil turi mavjud
+
+// 1 -> Inline JS => <button onclick="alert('salom dunyo')"> click me </button>
+
+// 2 -> Internal JS => <body> ... <script> alert('salom dunyo'); </script></body>
+
+// 3 -> External JS <script src="script.js"></script>
+
+console.log("Salom dunyo 3")
+
+//! Javascript bu Dasturlash Tili ECMAscript esa Specification (Versiyasi)
